c7d3dd642aaf04d00a683e5cfd7f3ed573355583
Hugh Simpson <hughrsimpson@gmail.com>
2014-08-27 18:41:11 +0100

    consider functions, don't show duplicates

--- src/reference_checker/main.clj
+++ src/reference_checker/main.clj
@@ -7,10 +7,13 @@
 ;              workflow-file (io/reader "resources/workflow.xml")]
     (let [mapping-file (slurp "resources/mappings.txt")
           workflow-file (slurp "resources/workflow.xml")
+          functions-file (slurp "resources/functions1.txt")
           mapping_regex #"\(\s*[^)(]+\s+/[^)(/]*/[^)(/]*/Sphonic/([^)(]+)\s*\)"
           mapping_matches (re-seq mapping_regex mapping-file)
-          mapping_refs (map #(nth % 1) mapping_matches)
-          orphan_refs (filter #(not (.contains workflow-file %)) mapping_refs)
+          mapping_refs (distinct (map #(nth % 1) mapping_matches))
+          orphan_refs (->> mapping_refs
+                        (filter #(not (.contains functions-file (str "$"%))) ,,,)
+                        (filter #(not (.contains workflow-file %)) ,,,))
           no_of_fact_refs (count mapping_matches)]
       (println no_of_fact_refs)
       orphan_refs))
