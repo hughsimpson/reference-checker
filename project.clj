560448f3db7bbe2fe7d947a801b7bf22285b3de3
Hugh Simpson <hughrsimpson@gmail.com>
2014-08-27 18:32:50 +0100

    project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject reference_checker "0.1.0-SNAPSHOT"
   :description "Produce a list of which references in a mapping are not created in a workflow xml"
-  :url "http://github.com/hughsimpson/z_values.git"
+  :url "http://github.com/hughsimpson/reference-checker.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main reference_checker.main
